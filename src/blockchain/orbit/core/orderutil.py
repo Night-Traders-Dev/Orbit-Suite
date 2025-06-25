20d5bb6557e7907c9dc4ddafe98782fc190483a8
Night-Traders-Dev <80061955+Night-Traders-Dev@users.noreply.github.com>
2025-06-25 17:11:43 -0400

    test**

--- src/blockchain/orbit/core/orderutil.py
+++ src/blockchain/orbit/core/orderutil.py
@@ -215,6 +215,7 @@ async def token_stats(token=TOKEN):
                 qty = data.get("amount")
                 sender = data.get("sender")
                 receiver = data.get("receiver")
+                supply = 0.0
                 if receiver == "ORB.BURN" or receiver == "ORB.00000000000000000000BURN":
                     try:
                         if meta_list[0].get("symbol") == tok:
