6709d69bdbea9579d2454ac4ffa63675604679f4
Night-Traders-Dev <80061955+Night-Traders-Dev@users.noreply.github.com>
2025-06-26 14:08:23 -0400

    TUI update**

--- src/node/go-node/core/ui.go
+++ src/node/go-node/core/ui.go
@@ -60,8 +60,7 @@ func StartTUI(node *OrbitNode) {
 
 	mainFlex := tview.NewFlex().
 		AddItem(left, 0, 3, true).
-		AddItem(logView, 0, 2, false)
-
+		AddItem(tview.NewTextView().SetText("Press [r] to refresh, [d] for detailed view"), 1, 0, false)
 	flex := tview.NewFlex().SetDirection(tview.FlexRow).
 		AddItem(header, 1, 1, false).
 		AddItem(mainFlex, 0, 1, true)
