92fa8ebece4424c465321486caa368110ab9279d
Night-Traders-Dev <80061955+Night-Traders-Dev@users.noreply.github.com>
2025-06-27 18:58:03 -0400

    test**

--- src/explorer/orbit_explorer.py
+++ src/explorer/orbit_explorer.py
@@ -574,6 +574,7 @@ def api_docs():
 
 @app.route("/api/chain")
 def api_chain():
+    nodefeebalance, _ = load_balance("ORB.3C0738F00DE16991DDD5B506")
     if is_thousand_milestone(len(g.chain)):
         #Get active nodes
         active_nodes = active_node_registry
@@ -587,7 +588,6 @@ def api_chain():
                 address = data.get("node", {}).get("address", "Unknown")
                 user = data.get("node", {}).get("user", "Unknown")
                 print(f"Node {node_id} last seen at {last_seen}, address: {address}, user: {user}")
-                nodefeebalance, _ = load_balance("ORB.3C0738F00DE16991DDD5B506")
                 print(f"Node Fee Balance: {nodefeebalance}")              
     return jsonify(g.chain)
 
