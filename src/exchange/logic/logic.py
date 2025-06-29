c7ecadc70a61d8455ef5b8eb240a8b5821ec799c
Night-Traders-Dev <80061955+Night-Traders-Dev@users.noreply.github.com>
2025-06-29 12:47:41 -0400

    Fix**

--- src/exchange/logic/logic.py
+++ src/exchange/logic/logic.py
@@ -171,10 +171,11 @@ async def swap_token(from_token, to_token, amount, receiver, sender):
                 current_price = (orbit_value / quantity)
                 if from_token == "ORBIT":
                     # Swap from ORBIT to token
-                    transfer_amount = (orbit_value / current_price)
-                else:
-                    # Swap from token to ORBIT
-                    transfer_amount = (current_price * amount)
+                    print(f"Swapping {amount} ORBIT to {to_token}")
+                    print(f"Current price of {to_token} is {current_price} Orbit")
+                    print(f"Orbit value of {orbit_value} for {quantity} {to_token}")
+                    print(f"(orbit_value / current_price) = {orbit_value / current_price}")
+                transfer_amount = (current_price * amount)
     token_tx = TXExchange.create_token_transfer_tx(
     sender=sender,
     receiver=receiver,
